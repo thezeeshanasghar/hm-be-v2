57092faaabb9d5264516f6c5947918ba247e16a5
thezeeshanasghar <thezeeshanasghar@gmail.com>
2018-09-03 10:42:33 +0500

    add installment page

---
+++ DB/dbo/Tables/Installment.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Installment]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [CarPurchaseID] BIGINT NOT NULL, 
+    [Amount] DECIMAL(19, 4) NOT NULL, 
+    [NextDueDate] DATE NOT NULL, 
+    [InstallmentDate] DATE NOT NULL
+	
+)
--- HM-API-V4/Controllers/CarPurchaseController.cs
+++ HM-API-V4/Controllers/CarPurchaseController.cs
@@ -12,56 +12,114 @@ namespace HM_API_V4.Controllers
 {
     public class CarPurchaseController : BaseController
     {
+        private HMEntities1 db = new HMEntities1();
+
+        public IEnumerable<CarPurchaseDTO> GetAll()
+        {
+            var dbObjs = db.CarPurchases.ToList();
+            List<CarPurchaseDTO> dtoObjs = Mapper.Map<List<CarPurchaseDTO>>(dbObjs);
+            return dtoObjs;
+        }
+
         public Response<CarPurchaseDTO> Post(CarPurchaseDTO cpDTO)
         {
             try
             {
+                CarPurchase cpDB = Mapper.Map<CarPurchase>(cpDTO);
+                cpDB.Buyers.Clear();
+                cpDB.Sellers.Clear();
+                cpDB.Witnesses.Clear();
 
-                using (HMEntities1 entities = new HMEntities1())
+                foreach (var witness in cpDTO.Witnesses)
                 {
-                    CarPurchase cpDB = Mapper.Map<CarPurchase>(cpDTO);
-                    cpDB.Buyers.Clear();
-                    cpDB.Sellers.Clear();
-                    cpDB.Witnesses.Clear();
-
-                    foreach (var witness in cpDTO.Witnesses)
-                    {
-                        var witnessDB = Mapper.Map<Witness>(witness);
-                        entities.Witnesses.Add(witnessDB);
-                        entities.SaveChanges();
-                        cpDB.Witnesses.Add(witnessDB);
-                    }
-                    entities.CarPurchases.Add(cpDB);
-                    entities.SaveChanges();
-                    cpDTO.Id = cpDB.Id;
-                    cpDTO.Witnesses = Mapper.Map<List<WitnessDTO>>(cpDB.Witnesses);
-                    foreach (var account in cpDTO.Sellers)
-                    {
-                        Account dbAccount = entities.Accounts.FirstOrDefault(s => s.Id == account.Id);
-                        cpDB.Sellers.Add(dbAccount);
-                        entities.SaveChanges();
-                    }
-                    cpDTO.Sellers = Mapper.Map<List<AccountDTO>>(cpDB.Sellers);
-                    foreach (var account in cpDTO.Buyers)
-                    {
-                        Account dbAccount = entities.Accounts.FirstOrDefault(s => s.Id == account.Id);
-                        cpDB.Buyers.Add(dbAccount);
-                        entities.SaveChanges();
-                    }
-                    cpDTO.Buyers = Mapper.Map<List<AccountDTO>>(cpDB.Buyers);
-
-
-                    var car = entities.Cars.FirstOrDefault(s => s.Id == cpDTO.CarID);
-                    car.Accounts = cpDB.Buyers;
-                    entities.SaveChanges();
-                    cpDB.Car = car;
-                    cpDTO.Car = Mapper.Map<CarDTO>(cpDB.Car);
-
-                    cpDTO.Id = cpDB.Id;
-
-
-                    return new Response<CarPurchaseDTO>(true, null, cpDTO);
+                    var witnessDB = Mapper.Map<Witness>(witness);
+                    db.Witnesses.Add(witnessDB);
+                    db.SaveChanges();
+                    cpDB.Witnesses.Add(witnessDB);
                 }
+                db.CarPurchases.Add(cpDB);
+                db.SaveChanges();
+                cpDTO.Id = cpDB.Id;
+                cpDTO.Witnesses = Mapper.Map<List<WitnessDTO>>(cpDB.Witnesses);
+                foreach (var account in cpDTO.Sellers)
+                {
+                    Account dbAccount = db.Accounts.FirstOrDefault(s => s.Id == account.Id);
+                    cpDB.Sellers.Add(dbAccount);
+                    db.SaveChanges();
+                }
+                cpDTO.Sellers = Mapper.Map<List<AccountDTO>>(cpDB.Sellers);
+                foreach (var account in cpDTO.Buyers)
+                {
+                    Account dbAccount = db.Accounts.FirstOrDefault(s => s.Id == account.Id);
+                    cpDB.Buyers.Add(dbAccount);
+                    db.SaveChanges();
+                }
+                cpDTO.Buyers = Mapper.Map<List<AccountDTO>>(cpDB.Buyers);
+
+
+                var car = db.Cars.FirstOrDefault(s => s.Id == cpDTO.CarID);
+                car.Accounts = cpDB.Buyers;
+                db.SaveChanges();
+                cpDB.Car = car;
+                cpDTO.Car = Mapper.Map<CarDTO>(cpDB.Car);
+
+                cpDTO.Id = cpDB.Id;
+
+                // sellers
+                Transaction t = new Transaction();
+                t.Account = cpDB.Sellers.ElementAt(0);
+                t.AccountID = t.Account.Id;
+                t.Amount = cpDB.Price;
+                t.Date = DateTime.Now.Date;
+                t.Description = "Sell Car " + cpDB.Car.RegistrationNumber;
+                db.Transactions.Add(t);
+                db.SaveChanges();
+
+                t = new Transaction();
+                t.Account = cpDB.Sellers.ElementAt(0);
+                t.AccountID = t.Account.Id;
+                t.Amount = -1 * cpDB.SellerCom;
+                t.Date = DateTime.Now.Date;
+                t.Description = "Pay commision for car " + cpDB.Car.RegistrationNumber;
+                db.Transactions.Add(t);
+
+                t = new Transaction();
+                t.Account = db.Accounts.ElementAt(0);
+                t.AccountID = t.Account.Id;
+                t.Amount = cpDB.SellerCom;
+                t.Date = DateTime.Now.Date;
+                t.Description = "Receive commision in deal of a car " + cpDB.Car.RegistrationNumber;
+                db.Transactions.Add(t);
+
+                // buyers
+                t = new Transaction();
+                t.Account = cpDB.Buyers.ElementAt(0);
+                t.AccountID = t.Account.Id;
+                t.Amount = -1 * cpDB.Price;
+                t.Date = DateTime.Now.Date;
+                t.Description = "Buy Car " + cpDB.Car.RegistrationNumber;
+                db.Transactions.Add(t);
+                db.SaveChanges();
+
+                t = new Transaction();
+                t.Account = cpDB.Sellers.ElementAt(0);
+                t.AccountID = t.Account.Id;
+                t.Amount = -1 * cpDB.BuyerCom;
+                t.Date = DateTime.Now.Date;
+                t.Description = "Pay commision for car " + cpDB.Car.RegistrationNumber;
+                db.Transactions.Add(t);
+
+                t = new Transaction();
+                t.Account = db.Accounts.ElementAt(0);
+                t.AccountID = t.Account.Id;
+                t.Amount = cpDB.BuyerCom;
+                t.Date = DateTime.Now.Date;
+                t.Description = "Receive commision in deal of a car " + cpDB.Car.RegistrationNumber;
+                db.Transactions.Add(t);
+
+
+
+                return new Response<CarPurchaseDTO>(true, null, cpDTO);
             }
             catch (Exception e)
             {
